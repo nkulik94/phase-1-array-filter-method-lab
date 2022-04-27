7d328e6ddac92c90bcb5ead5111f6614850f8cd3
Naftali Kulik <nkulik1994@gmail.com>
2022-04-27 16:16:41 -0400

    commit tree f6209725aab76cbf682579d34d62d285962d60d5

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(list, name) {
+    const rightName = list.filter(function(n) {
+        return n.toUpperCase() === name.toUpperCase()
+    })
+    return rightName
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(d) {
+        for (const l in letters) {
+            return d[l] === letters[l]
+        }
+    })
+}
+function matchName(drivers, name) {
+    return drivers.filter(function(d) {
+        for (let i = 0; i < Object.values(d).length; i++) {
+        return Object.values(d)[i] === name
+        }
+    })
+}
